96447ceb5ace929cabf6923608c21930349f01b3
Vasudha Tapriya <vasudhatapriya2@gmail.com>
2020-10-27 16:24:00 +0530

    edited code added

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+export default {
+  products: [
+  {
+      _id:'1',
+      name: 'Rubber Timing Belts',
+      Category: 'Wrap Construction Belts',
+      image: "/images/TB-1.jpg",
+      brand: "Nitta",
+      price: 'Rs. 100',
+      description: 'Used for heavy machinery only, can take load upto 20N'
+
+  },
+  {
+      _id:'2',
+      name: 'Classical Section Belts',
+      Category: 'Wrap Construction Belts',
+      image: "/images/TB-2.jpg",
+      brand: "PIX",
+      price: 'Rs. 900'
+  },
+  {
+      _id:'3',
+      name: 'Wedge Section Belts',
+      Category: 'Banded Belts',
+      image: "/images/TB-3.jpg",
+      brand: "Nitta",
+      price: 'Rs. 800'
+  },
+  {
+      _id:'4',
+      name: 'Narrow Section Belts',
+      Category: 'Special Construction Belts',
+      image: "/images/TB-4.jpg",
+      brand: "Nitta",
+      price: 'Rs. 300'
+  },
+  {
+      _id:'5',
+      name: 'Light Duty Single V Belts',
+      Category: 'Special Construction Belts',
+      image: "/images/TB-5.jpg",
+      brand: "Nitta",
+      price: 'Rs. 500'
+  },
+  ]
+}
